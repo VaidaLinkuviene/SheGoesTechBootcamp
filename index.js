aaf257be5e1242bb1d61e8df7596851d65adc76f
Vaida <linkuviene.vaida@gmail.com>
2023-06-13 09:32:03 +0300

    Sort, map, filter, every, some, reduce functions

--- index.js
+++ index.js
@@ -1,35 +1,106 @@
-// FIRST FUNCTION
+let arr = [9, 8, 5, 6, 1, 0, 3, 2, 4, 8, 9, 6, 5];
 
-const ifStatements = () =>{
-if(amount < 0){
-    console.log("Less than zero");
-} else if(amount>0){
-    console.log("More than zero");
-}else{
-    console.log("Zero")
+// MAP
+
+const doubleNumber = () => {
+  let doubleItems = [];
+  arr.forEach((item) => {
+    doubleItems.push(item * 2);
+  });
+  return doubleItems;
+};
+
+const mapDoubleNumber = () => {
+  let result = arr.map((item) => item * 2);
+  return result;
+};
+
+// Filter
+
+const filterNumbers = () => {
+  let doubleItems = [];
+  arr.forEach((item) => {
+    if (item > 5) {
+      doubleItems.push(item);
+    }
+  });
+  return doubleItems;
+};
+
+const filterFilterNumbers = () => {
+  let result = arr.filter((item) => item > 5);
+  return result;
+};
+
+// Every
+
+const everyNumbers = () => {
+    let result = true; 
+    for(let i=0; i<arr.length; i++){
+      if(arr[i]<=0){
+        result = false;
+        break;
+      }
+    }
+return result;
 }
-return amount;
+
+const everyEveryNumbers = () => {
+  let result = arr.every((item) => item > 0);
+  return result;
+};
+
+// SOME
+
+const someNumbers = () => {
+  let result = false; 
+  for(let i=0; i<arr.length; i++){
+    if(arr[i]%2 ===0){
+      result = true;
+      break;
+    }
+  }
+return result;
 }
+  
+  const someSomeNumbers = () => {
+    let result = arr.some((item) => item%2 === 0);
+    return result;
+  };
 
-// SECOND FUNCTION
+  // REDUCE
 
-const functionWithArguments = (firstName, lastName, age) =>{
-    console.log(firstName.toUpperCase());
-    console.log(lastName.slice(0, 1));
-    console.log(age.toString());
-}
+  const reduceNumbers = () => {
+    sum = 0;
+    for(let i=0; i<arr.length; i++){
+      sum = sum + arr[i];
+    }
+    return sum;
+  };
+  
+  const reduceReduceNumbers = () => {
+    let result = arr.reduce((initial ,item) => initial + item, 0);
+    return result;
+  };
 
-functionWithArguments("Vaida", "Linkuviene", 34);
+  // SORT
 
-// THIRD FUNCTION
-
-const animal = {
-    species: "cat",
-    name: "Micy",
-    age: 4,
-}
-
-const objectFunction = () => {
-   let catsName =  `My ${animal.species}'s name is ${animal.name}`;
-   return catsName;
-}
+  const sortNumbers = () => {
+    for(let i=1; i<arr.length; i++){
+      for(let j=0; j<i; j++){
+        if(arr[i]<arr[j]){
+          let number = arr[i];
+          arr[i] = arr[j];
+          arr[j]= number;
+        }
+      }
+    }
+    return arr;
+  };
+  console.log("sortNumbers", sortNumbers());
+  
+  const sortSortNumbers = () => {
+    arr.sort();
+    return arr;
+  };
+  console.log("sortSortNumbers", sortSortNumbers());
